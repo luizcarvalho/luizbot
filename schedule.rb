a538fa7f2b4e2b7bd68fda69f4db08a0ed470d6a
Luiz Carvalho <maximusmano@gmail.com>
2018-07-03 08:25:21 -0300

    add work interval

--- schedule.rb
+++ schedule.rb
@@ -1,7 +1,12 @@
 require 'dotenv/load'
 require 'telegram/bot'
 
-response = Faraday.get ENV['CAMBIO_ENDPOINT']
-euro_price = JSON.parse(response.body)['EUR']['RateToBRL']
-bot = Telegram::Bot::Client.new(ENV['TELEGRAM_TOKEN'])
-bot.api.send_message(chat_id: 51112220, text: " 💶 EURO: R$ #{euro_price}")
+START_HOUR = 8
+END_HOUR = 18
+
+if START_HOUR <= Time.now.hour && Time.now.hour <= END_HOUR
+  response = Faraday.get ENV['CAMBIO_ENDPOINT']
+  euro_price = JSON.parse(response.body)['EUR']['RateToBRL']
+  bot = Telegram::Bot::Client.new(ENV['TELEGRAM_TOKEN'])
+  bot.api.send_message(chat_id: 51112220, text: " 💶 EURO: R$ #{euro_price}")
+end
