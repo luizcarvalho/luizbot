a9f9e7aed5ab987876752a0e58fbb7d9a4fc02d0
Luiz Carvalho <maximusmano@gmail.com>
2018-08-28 13:58:57 -0300

    fix conflicts

--- schedule.rb
+++ schedule.rb
@@ -3,7 +3,7 @@ require 'telegram/bot'
 
 START_HOUR = 8
 END_HOUR = 18
-<<<<<<< HEAD
+
 quantidade = ENV["QUANTIDADE"].to_i || 660
 IOF = ENV["IOF"].to_f || 0.0638
 
@@ -13,19 +13,9 @@ if START_HOUR <= Time.now.hour && Time.now.hour <= END_HOUR
   bot = Telegram::Bot::Client.new(ENV['TELEGRAM_TOKEN'])
 
   resultado = "💶 EURO: R$ #{euro_price} \n"\
-    "🔢 Quantidade #{quantidade}\n\n"\
-    "#️⃣ Valor #{euro_price * quantidade} \n"\
-    "📶 TOTAL #{(euro_price + (euro_price * IOF)) * quantidade} "
+              "🔢 Quantidade #{quantidade}\n"\
+              "#️⃣ Valor #{euro_price * quantidade} \n\n"\
+              "🔰 TOTAL: R$  #{((euro_price + (euro_price * IOF)) * quantidade).round(2)}"
 
   bot.api.send_message(chat_id: ENV['OWNER_ID'], text: resultado)
 end
-
-=======
-
-if START_HOUR <= Time.now.hour && Time.now.hour <= END_HOUR
-  response = Faraday.get ENV['CAMBIO_ENDPOINT']
-  euro_price = JSON.parse(response.body)['EUR']['RateToBRL']
-  bot = Telegram::Bot::Client.new(ENV['TELEGRAM_TOKEN'])
-  bot.api.send_message(chat_id: 51112220, text: " 💶 EURO: R$ #{euro_price}")
-end
->>>>>>> 4264453dd3a960155abb53d8e060788ef3ca0c9e
