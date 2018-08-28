0ef289a3e7413b074084ef0a8dde26ec1e81e508
Luiz Carvalho <contato@luizcarvalho.com>
2018-08-28 10:34:15 -0300

    more information about euro

--- schedule.rb
+++ schedule.rb
@@ -3,10 +3,19 @@ require 'telegram/bot'
 
 START_HOUR = 8
 END_HOUR = 18
+quantidade = ENV["QUANTIDADE"].to_i || 660
+IOF = ENV["IOF"].to_f || 0.0638
 
 if START_HOUR <= Time.now.hour && Time.now.hour <= END_HOUR
   response = Faraday.get ENV['CAMBIO_ENDPOINT']
-  euro_price = JSON.parse(response.body)['EUR']['RateToBRL']
+  euro_price = JSON.parse(response.body)['EUR']['RateToBRL'].to_f
   bot = Telegram::Bot::Client.new(ENV['TELEGRAM_TOKEN'])
-  bot.api.send_message(chat_id: ENV['OWNER_ID'], text: " 💶 EURO: R$ #{euro_price}")
+
+  resultado = "💶 EURO: R$ #{euro_price} \n"\
+    "🔢 Quantidade #{quantidade}\n\n"\
+    "#️⃣ Valor #{euro_price * quantidade} \n"\
+    "📶 TOTAL #{(euro_price + (euro_price * IOF)) * quantidade} "
+
+  bot.api.send_message(chat_id: ENV['OWNER_ID'], text: resultado)
 end
+
