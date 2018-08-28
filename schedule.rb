85dac218650b3834e12cf68e1a6a74b453cddcf6
Luiz Carvalho <contato@luizcarvalho.com>
2018-08-28 10:23:07 -0300

    remove test user

--- schedule.rb
+++ schedule.rb
@@ -8,5 +8,5 @@ if START_HOUR <= Time.now.hour && Time.now.hour <= END_HOUR
   response = Faraday.get ENV['CAMBIO_ENDPOINT']
   euro_price = JSON.parse(response.body)['EUR']['RateToBRL']
   bot = Telegram::Bot::Client.new(ENV['TELEGRAM_TOKEN'])
-  bot.api.send_message(chat_id: 51112220, text: " 💶 EURO: R$ #{euro_price}")
+  bot.api.send_message(chat_id: ENV['OWNER_ID'], text: " 💶 EURO: R$ #{euro_price}")
 end
