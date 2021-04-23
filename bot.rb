2e8710a76396d9827fdf2b38028d4d7ab745f298
Luiz Carvalho <maximusmano@gmail.com>
2021-04-23 12:11:01 -0300

    discard message if text are empty

--- bot.rb
+++ bot.rb
@@ -7,13 +7,12 @@ require_relative './config/routes'
 require_relative './lib/luiz_bot'
 require_relative './lib/utils'
 
-
 logger = Logger.new(STDOUT)
 
 Telegram::Bot::Client.run(ENV['TELEGRAM_TOKEN'], logger: logger) do |bot|
-
   bot.listen do |message|
-    # puts "#{message.from.first_name}[#{message.chat.id}]: #{message.text}"
-    LuizBot::Router.dispatch(bot, message) if message.chat.id == ENV['OWNER_ID'].to_i
+    puts "#{message.from.first_name}[#{message.chat.id}]: #{message.text}"
+
+    LuizBot::Router.dispatch(bot, message) if (message.text != '') && (message.chat.id == ENV['OWNER_ID'].to_i)
   end
 end
